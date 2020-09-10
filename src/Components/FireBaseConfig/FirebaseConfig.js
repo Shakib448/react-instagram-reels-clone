7ea1c669da0dc3d43eb5f26691eaa8ffc9699aa9
Shakib448 <shakiba448@gmail.com>
2020-09-10 16:50:58 +0600

    React Instagram Reels Clone

--- src/Components/FireBaseConfig/FirebaseConfig.js
+++ src/Components/FireBaseConfig/FirebaseConfig.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase'
 
 // use your firebase app config here
-const firebaseConfig = {
-    apiKey: "AIzaSyCWjpR4P5GGh445klpFBLusZ_MTUNrspG4",
-    authDomain: "react-instagram-reels-clone.firebaseapp.com",
-    databaseURL: "https://react-instagram-reels-clone.firebaseio.com",
-    projectId: "react-instagram-reels-clone",
-    storageBucket: "react-instagram-reels-clone.appspot.com",
-    messagingSenderId: "167393851506",
-    appId: "1:167393851506:web:791a9aa2ba29d7435b6aeb",
-    measurementId: "G-LNVYJGVZSH"
-};
+
 
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
